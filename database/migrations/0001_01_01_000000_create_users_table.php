ce4a1dddfd6a31223b863257443be654e831a542
Caio Reis <caiofreis2005@gmail.com>
2025-07-15 20:14:51 -0300

    feat(migrations): create database tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('cpf')->unique();
             $table->string('password');
+            $table->date('birth_date');
+            $table->string('phone');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_07_15_224119_create_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->date('appointment_date');
+            $table->enum('status', ['scheduled', 'completed', 'canceled'])->default('scheduled');
+            $table->decimal('value', 10, 2);
+            $table->timestamps();
+
+            $table->foreignId('doctor_id')->constrained('doctors')->onDelete('cascade');
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->foreignId('receptionist_id')->nullable()->constrained('receptionists')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
