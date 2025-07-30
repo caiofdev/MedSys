bb146dd69d77fe5b296b7e71580ff0b0fa13d814
Caio Reis <caiofreis2005@gmail.com>
2025-07-30 10:44:21 -0300

    feat(routes): add admin routes for doctors and receptionists management

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,19 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\ReceptionistController;
 
 Route::get('/', function () {
     return redirect()->route('login');
 })->name('home');
 
 
-Route::get('/doctor-table', function () {
-    return Inertia::render('tables/doctor-table');
-})->name('doctor-table');
 
 Route::get('/patient-table', function () {
     return Inertia::render('tables/patient-table');
 })->name('patient-table');
 
-Route::get('/receptionist-table', function () {
-    return Inertia::render('tables/receptionist-table');
-})->name('receptionist-table');
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -28,6 +24,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     
     Route::get('admin/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard')->middleware('user.type:admin');
     Route::get('admin/admins', [AdminController::class, 'index'])->name('admin.table')->middleware('user.type:admin');
+    Route::get('admin/doctors', [DoctorController::class, 'index'])->name('doctor-table')->middleware('user.type:admin');
+    Route::get('admin/receptionists', [ReceptionistController::class, 'index'])->name('receptionist-table')->middleware('user.type:admin');
 
 
     Route::get('doctor/dashboard', [DashboardController::class, 'doctorDashboard'])->name('doctor.dashboard')->middleware('user.type:doctor');
