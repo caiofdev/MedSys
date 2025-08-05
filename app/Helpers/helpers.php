cf8396cd64c8cb5481f558d734d1a7bf251da741
Caio Reis <caiofreis2005@gmail.com>
2025-08-05 11:40:14 -0300

    refactor(helpers): remove unused getUniqueModelId function and clean up file

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,35 +1,5 @@
 <?php
 
-if (!function_exists('getUniqueModelId')) {
-    /**
-     * Returns a unique ID from a model, avoiding repetitions
-     *
-     * @param string $modelClass Name of the Model class (e.g., 'App\Models\Consultation')
-     * @param string $cacheKey Unique key used to cache already used IDs (e.g., 'consultation_prescription')
-     * @return mixed
-     */
-    function getUniqueModelId($modelClass, $cacheKey)
-    {
-        static $usedIds = [];
+namespace App\Helpers;
 
-        if (!isset($usedIds[$cacheKey])) {
-            $usedIds[$cacheKey] = [];
-        }
-
-        $availableIds = $modelClass::pluck('id')
-            ->diff($usedIds[$cacheKey])
-            ->values()
-            ->toArray();
-
-        if (empty($availableIds)) {
-            $usedIds[$cacheKey] = [];
-            $availableIds = $modelClass::pluck('id')->toArray();
-        }
-
-        $selectedId = fake()->randomElement($availableIds);
-
-        $usedIds[$cacheKey][] = $selectedId;
-        
-        return $selectedId;
-    }
-}
+// This file is intentionally left empty
